13767f533b1de329c4f64cc8eea4ce1f2e4db54b
Aspect13 <ad13box@gmail.com>
2023-08-10 21:11:08 +0400

    adding more logs to event

--- events/config.py
+++ events/config.py
@@ -9,6 +9,7 @@ class Event:
     def handle_new_ai_user(self, context, event, payload: dict):
         # payload == {user_id: int, user_email: str}
         if payload.get('user_email', '').endswith('@epam.com'):
+            log.info('Adding epam user to project %s', payload)
             secrets = VaultClient().get_all_secrets()
             try:
                 ai_project_id = secrets['ai_project_id']
@@ -26,6 +27,7 @@ class Event:
                     log.critical('Secret for "ai_project_roles" is not set')
                     return
             ai_project_roles = [i.strip() for i in ai_project_roles.split(',')]
+            log.info('Adding epam user %s to project %s with roles %s', payload, ai_project_id, ai_project_roles)
 
             context.rpc_manager.call.admin_add_user_to_project(
                 ai_project_id, payload['user_id'], ai_project_roles
