fbf1e6845b66cbee0348d54f1de9d206cb557c52
Aspect13 <ad13box@gmail.com>
2023-09-20 17:14:11 +0400

    formalized sio events validation results are passed into execution now no more pre-pre-pre formatting of payload no more validation errors that are not errors

--- flows.py
+++ flows.py
@@ -11,19 +11,18 @@ from .utils.ai_providers import AIProvider
     display_name='Prompt',
     tooltip='AI Predict prompt',
     icon_url='/flows/static/icons/prompt.svg',
-    # validation_rpc='embeddings_deduplicate_flow_validate'
 )
-def prompt(flow_context: dict, clean_data: dict, **kwargs):
+def prompt(flow_context: dict, clean_data: PredictPostModel, **kwargs):
     project_id = flow_context.get('project_id')
     # model_settings = data.integration_settings.dict(exclude={'project_id'}, exclude_unset=True)
-    model_settings = clean_data['integration_settings']
-    model_settings.pop('project_id', None)
-    integration_uid = clean_data['integration_uid']
-    input_ = clean_data['input_']
-    prompt_id = clean_data['prompt_id']
-    context = clean_data['context']
-    examples = clean_data['examples']
-    variables = clean_data['variables']
+    # model_settings = clean_data['integration_settings']
+    # model_settings.pop('project_id', None)
+    # integration_uid = clean_data['integration_uid']
+    # input_ = clean_data['input_']
+    # prompt_id = clean_data['prompt_id']
+    # context = clean_data['context']
+    # examples = clean_data['examples']
+    # variables = clean_data['variables']
 
     # data = dict(kwargs)
     # data['input'] = data['prompt_input']
@@ -46,22 +45,22 @@ def prompt(flow_context: dict, clean_data: dict, **kwargs):
     try:
         integration = AIProvider.get_integration(
             project_id=project_id,
-            integration_uid=integration_uid,
+            integration_uid=clean_data.integration_uid,
         )
         text_prompt = rpc_tools.RpcMixin().rpc.call.prompts_prepare_text_prompt(
-            project_id, prompt_id, input_,
-            context, examples, variables
+            project_id, clean_data.prompt_id, clean_data.input_,
+            clean_data.context, clean_data.examples, clean_data.variables
         )
     except Exception as e:
         log.error(str(e))
         return {"ok": False, "error": str(e)}
 
-    output = AIProvider.predict(project_id, integration, model_settings, text_prompt)
+    output = AIProvider.predict(project_id, integration, clean_data.integration_settings, text_prompt)
     if not output['ok']:
         return output
     return {"ok": True, "result": output['response']}
 
 
 @flow_tools.validator(flow_uid='prompt')
-def prompt_validate(**kwargs):
+def prompt_validate(**kwargs) -> PredictPostModel:
     return PredictPostModel.validate(kwargs)
